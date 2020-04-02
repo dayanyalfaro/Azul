3b9d42c482336a124835bef1f1f0ff443cdea22a
daly <dalianysperez@gmail.com>
2020-04-02 05:54:51 -0400

    initial environment created

--- player.pl
+++ player.pl
@@ -2,13 +2,12 @@
 
 :- dynamic(player_score/2, player_wall/2, player_stair/2, player_penalty/2).
 
-%-----------------Player ATTR
+%-----------------Player ATTR----------------------------
 set_score(ID, SCORE) :- ( (retract(player_score(ID, _)), !) ; true ) , assert(player_score(ID, SCORE)).
-set_wall(ID, WALL):- ( (retract(player_wall(ID, _)), !) ; true ) , assert(player_wall(ID, WALL)).
-set_stair(ID, STAIR):- ( (retract(player_stair(ID, _)), !) ; true ) , assert(player_stair(ID, STAIR)).
 set_penalty(ID, PENALTY):- ( (retract(player_penalty(ID, _)), !) ; true ) , assert(player_penalty(ID, PENALTY)).
 
-%-----------------Player Initialization
-init_player(ID):- set_score(ID, 0), init_matrix_wall(ID), init_stair(ID), set_penalty(ID, 0). 
+%-----------------Player Initialization--------------------
+init_player(ID):- set_score(ID, 0), init_board(ID), set_penalty(ID, 0). 
 
-%-----------------Player Actions
+%-----------------Player Actions---------------------------
+
